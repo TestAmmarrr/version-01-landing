b13c7fea84df85601a833828b757d9f1178cff0e
ammar <ammar@LAPTOP-7OGP3F1K>
2023-09-03 16:04:00 +0500

    configure tailwind intelli sense.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="ml-5">
+        <p className="text-sm font-medium text-gray-900">Muhammad Ammar</p>
+        <p className="text-sm text-gray-500">emr@gmail.com</p>
+      </div>
     </div>
   );
 }
